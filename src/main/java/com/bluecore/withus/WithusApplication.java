87400f3dbcf5a69816859d2b2bfde51b7f3caedf
pantera <pantera@blue-core.com>
2020-08-06 15:30:20 +0900

    initial commit

---
+++ src/main/java/com/bluecore/withus/WithusApplication.java
@@ -0,0 +1,11 @@
+package com.bluecore.withus;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WithusApplication {
+	public static void main(String[] args) {
+		SpringApplication.run(WithusApplication.class, args);
+	}
+}
---
+++ src/test/java/com/bluecore/withus/WithusApplicationTests.java
@@ -0,0 +1,11 @@
+package com.bluecore.withus;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WithusApplicationTests {
+	@Test
+	void contextLoads() {
+	}
+}
