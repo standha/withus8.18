f505c11c5c65d004cc0cea7ca08aa569a3706115
pantera <pantera@blue-core.com>
2020-08-10 18:25:15 +0900

    Scheduler 구현 (firebase에서 처리 가능할 경우 변경 필요)

--- src/main/java/com/bluecore/withus/repository/alarms/AppointmentRepository.java
+++ src/main/java/com/bluecore/withus/repository/alarms/AppointmentRepository.java
@@ -1,16 +1,25 @@
 package com.bluecore.withus.repository.alarms;
 
 import java.time.LocalDate;
+import java.time.LocalTime;
 import java.util.List;
 import java.util.Optional;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.lang.NonNull;
 
 import com.bluecore.withus.entity.User;
 import com.bluecore.withus.entity.alarms.Appointment;
 
 public interface AppointmentRepository extends JpaRepository<Appointment, Long> {
+	@NonNull
 	List<Appointment> findAllByUser(User user);
+	@NonNull
 	List<Appointment> findAllByUserAndDateIsBetween(User user, LocalDate startInclusive, LocalDate endInclusive);
+	@NonNull
+	List<Appointment> findAllByEnabledIsTrueAndDate(LocalDate date);
+	@NonNull
+	List<Appointment> findAllByEnabledIsTrueAndDateAndTime(LocalDate date, LocalTime time);
+
 	Optional<Appointment> findTopByUserAndDateOrderByIdDesc(User user, LocalDate date);
 }
