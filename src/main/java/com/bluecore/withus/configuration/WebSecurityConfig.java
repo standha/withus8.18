d0514e5056f300c149d2b8483712e8d9a09c0817
park sujin <atena9604@blue-core.com>
2020-08-11 09:44:21 +0900

    로그인 기능 개발 중

--- src/main/java/com/bluecore/withus/configuration/WebSecurityConfig.java
+++ src/main/java/com/bluecore/withus/configuration/WebSecurityConfig.java
@@ -1,5 +1,6 @@
 package com.bluecore.withus.configuration;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
@@ -10,19 +11,29 @@ import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 
+import com.bluecore.withus.service.UserService;
+
 @Configuration
 @EnableWebSecurity
 public class WebSecurityConfig extends WebSecurityConfigurerAdapter{
+	private final UserService userService;
+
+	@Autowired
+	public WebSecurityConfig(UserService userService) {
+		this.userService = userService;
+	}
+
 	@Override
 	protected void configure(HttpSecurity http) throws Exception {
 	http
 		.authorizeRequests()
-		.antMatchers( "/", "/home").permitAll()
+		.antMatchers( "/", "LogIn/**", "/css/**", "/js/**", "/images/**","/**").permitAll()
 			.anyRequest().authenticated()
 			.and()
 		.formLogin()
 			.loginPage("/login")
 			.permitAll()
+			.defaultSuccessUrl("/home")
 			.and()
 		.logout()
 			.permitAll();
@@ -31,13 +42,14 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter{
 	@Bean
 	@Override
 	public UserDetailsService userDetailsService() {
-		UserDetails user =
+		/*UserDetails user =
 			User.withDefaultPasswordEncoder()
 				.username("user")
-				.password("password")
+				.password(user.getPassword())
 				.roles("USER")
 				.build();
 
-		return new InMemoryUserDetailsManager(user);
+		return new InMemoryUserDetailsManager(user);*/
+		return userService;
 	}
 }
--- src/main/java/com/bluecore/withus/service/UserService.java
+++ src/main/java/com/bluecore/withus/service/UserService.java
@@ -2,13 +2,16 @@ package com.bluecore.withus.service;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.lang.Nullable;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
 
 import com.bluecore.withus.entity.User;
 import com.bluecore.withus.repository.UserRepository;
 
 @Service
-public class UserService {
+public class UserService implements UserDetailsService {
 	private final UserRepository userRepository;
 
 	@Autowired
@@ -19,5 +22,20 @@ public class UserService {
 	@Nullable
 	public User getUserById(String id) {
 		return userRepository.findById(id).orElse(null);
+	}
+
+	@Nullable
+	public User saveUser(User user) {
+		return userRepository.save(user);
+	}
+
+	@Override
+	public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+		User user = getUserById(username);
+		if (user == null) {
+			throw new UsernameNotFoundException(String.format("Username (%s) does not exist.", username));
+		}
+
+		return user;
 	}
 }
