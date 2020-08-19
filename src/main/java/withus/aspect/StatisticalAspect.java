f34f6fccde8de5a41289e3d100bd8c75a1ef9d16
pantera <pantera@blue-core.com>
2020-08-19 17:14:01 +0900

    AspectJ: @Around → @Before

--- src/main/java/withus/aspect/StatisticalAspect.java
+++ src/main/java/withus/aspect/StatisticalAspect.java
@@ -1,8 +1,8 @@
 package withus.aspect;
 
-import org.aspectj.lang.ProceedingJoinPoint;
-import org.aspectj.lang.annotation.Around;
+import org.aspectj.lang.JoinPoint;
 import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.Before;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Component;
@@ -13,19 +13,13 @@ public class StatisticalAspect {
 	private static final Logger logger = LoggerFactory.getLogger(StatisticalAspect.class);
 
 	/**
-	 * Gets invoked around methods annotated with {@link withus.aspect.Statistical}.
+	 * Gets invoked before methods annotated with {@link withus.aspect.Statistical}.
 	 */
-	@Around("@annotation(withus.aspect.Statistical)")
-	public Object updateStatisticsAround(ProceedingJoinPoint proceedingJoinPoint) throws Throwable {
-		Object result = null;
-		try {
-			// TODO: UPDATE THE STATISTICS HERE.
-			logger.info("TODO: UPDATE THE STATISTICS HERE.");
-			result = proceedingJoinPoint.proceed();
-		} catch (Exception exception) {
-			logger.error(exception.getLocalizedMessage(), exception);
-		}
+	@Before("@annotation(withus.aspect.Statistical)")
+	public void updateStatisticsAround(JoinPoint joinPoint) {
+		logger.info("{}: About to update the statistics.", joinPoint.getSignature());
 
-		return result;
+		// TODO: UPDATE THE STATISTICS HERE.
+		logger.info("TODO: UPDATE THE STATISTICS HERE.");
 	}
 }
