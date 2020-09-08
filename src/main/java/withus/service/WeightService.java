c47879b9229d2ced70ebab488dca335eec641237
Oh JooEun <ojeun@blue-core.com>
2020-09-08 18:12:52 +0900

    체중 입력

---
+++ src/main/java/withus/service/WeightService.java
@@ -0,0 +1,43 @@
+package withus.service;
+
+import lombok.NonNull;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import withus.entity.RecordKey;
+import withus.entity.Tbl_weight;
+import withus.repository.WeightRecordRepository;
+
+import java.time.LocalDate;
+import java.util.List;
+
+@Service
+public class WeightService {
+    private final WeightRecordRepository weightRecordRepository;
+
+    @Autowired
+    public WeightService(WeightRecordRepository weightRecordRepository){
+        this.weightRecordRepository = weightRecordRepository;
+    }
+
+    @NonNull
+    public Tbl_weight upsertWeightRecord(Tbl_weight tbl_weight){
+        Tbl_weight saved = weightRecordRepository.save(tbl_weight);
+        return saved;
+    }
+
+    @NonNull
+    public List<Tbl_weight> getWeightRecord(String id, float weight){
+        return weightRecordRepository.findByPk_IdAndWeightGreaterThan(id, weight);
+    }
+
+    @NonNull
+    public List<Tbl_weight> getWeightToday(LocalDate localDate, float weight){
+        return weightRecordRepository.findByPk_DateAndWeightGreaterThan(localDate, weight);
+    }
+
+    @NonNull
+    public List<Tbl_weight> getWeightDateRecord(RecordKey pk, float weight){
+        return weightRecordRepository.findByPkAndWeightGreaterThan(pk, weight);
+    }
+
+}
