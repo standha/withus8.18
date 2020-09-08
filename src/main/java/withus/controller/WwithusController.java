1d711fd26453f2f7e4f5414e8ca220e2f3e2a762
pantera <pantera@blue-core.com>
2020-09-08 12:31:10 +0900

    /wwithus/request-chat-balloon 대충 구현 renaming:  - BaseController#logger → log (lombok과의 통일성)  - WwithusService#getWwithusEntry → getWwithusEntryAndSaveHistory

--- src/main/java/withus/controller/WwithusController.java
+++ src/main/java/withus/controller/WwithusController.java
@@ -2,22 +2,23 @@ package withus.controller;
 
 import java.util.List;
 
-import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.servlet.ModelAndView;
 import withus.auth.AuthenticationFacade;
 import withus.dto.Result;
 import withus.dto.wwithus.ChatBalloon;
+import withus.dto.wwithus.WwithusEntryRequest;
 import withus.entity.User;
+import withus.entity.WwithusEntry;
 import withus.entity.WwithusEntryHistory;
 import withus.service.UserService;
 import withus.service.WwithusService;
 
 @Controller
-@Slf4j
 public class WwithusController extends BaseController {
 	private final WwithusService wwithusService;
 
@@ -39,9 +40,10 @@ public class WwithusController extends BaseController {
 	@GetMapping("/wwithus/chat-balloons")
 	@ResponseBody
 	public Result<List<ChatBalloon>> getChatBalloons() {
-		User user = getUser();
 		Result.Code code = Result.Code.OK;
 		List<ChatBalloon> data = null;
+
+		User user = getUser();
 		List<WwithusEntryHistory> wwithusEntryHistories = wwithusService.getWwithusEntryHistories(user);
 		if (!wwithusEntryHistories.isEmpty()) {
 			log.debug("User ({}) has {} history(ies) of today.", user, wwithusEntryHistories.size());
@@ -49,6 +51,29 @@ public class WwithusController extends BaseController {
 		}
 
 		return Result.<List<ChatBalloon>>builder()
+			.code(code)
+			.data(data)
+			.build();
+	}
+
+	@GetMapping("/wwithus/request-chat-balloon")
+	@ResponseBody
+	public Result<ChatBalloon> getChatBalloon(@RequestBody WwithusEntryRequest wwithusEntryRequest) {
+		Result.Code code = Result.Code.ERROR;
+		ChatBalloon data = null;
+
+		try {
+			User user = getUser();
+			wwithusEntryRequest.setUser(user);
+			WwithusEntry wwithusEntry = wwithusService.getWwithusEntryAndSaveHistory(wwithusEntryRequest);
+
+			code = Result.Code.OK;
+			data = wwithusService.toChatBalloon(wwithusEntry);
+		} catch (Exception exception) {
+			log.error(exception.getLocalizedMessage(), exception);
+		}
+
+		return Result.<ChatBalloon>builder()
 			.code(code)
 			.data(data)
 			.build();
