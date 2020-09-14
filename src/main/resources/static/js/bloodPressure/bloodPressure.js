73694ae7bdb9045376c1ba85e724db722c8ff81c
양찬홍 <chyang@blue-core.com>
2020-09-14 14:34:52 +0900

    혈압맥박 예외처리

--- src/main/resources/static/js/bloodPressure/bloodPressure.js
+++ src/main/resources/static/js/bloodPressure/bloodPressure.js
@@ -1,7 +1,15 @@
 function onFormSubmission(form) {
-    const contraction = form.querySelector("input[name=contraction]").value;
-    const relaxation = form.querySelector("input[name=relaxation]").value;
-    const pressure = form.querySelector("input[name=pressure]").value;
+    if(form.querySelector("input[name=contraction]").value=="" ||
+        form.querySelector("input[name=relaxation]").value=="" ||
+        form.querySelector("input[name=pressure]").value=="")
+    {
+        alert("모든 항목에 입력 ");
+    }
+    else{
+        const contraction = form.querySelector("input[name=contraction]").value;
+        const relaxation = form.querySelector("input[name=relaxation]").value;
+        const pressure = form.querySelector("input[name=pressure]").value;
+    }
     const body = {
         contraction: contraction,
         relaxation: relaxation,
