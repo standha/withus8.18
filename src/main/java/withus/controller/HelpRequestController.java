879abef1dfd1dd9f460ad87047416464ce723c7e
sun <dariouse@blue-core.com>
2020-10-07 09:59:44 +0900

    추가
    Signed-off-by: sun <dariouse@blue-core.com>

---
+++ src/main/java/withus/controller/HelpRequestController.java
@@ -0,0 +1,56 @@
+package withus.controller;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.ResponseBody;
+import withus.auth.AuthenticationFacade;
+import withus.dto.Result;
+import withus.entity.*;
+import withus.service.CountService;
+import withus.service.HelperRequestService;
+import withus.service.UserService;
+
+import javax.servlet.http.HttpServletRequest;
+import java.time.LocalDate;
+import java.time.LocalTime;
+
+@Controller
+public class HelpRequestController extends BaseController{
+    protected final Logger logger = LoggerFactory.getLogger(getClass());
+    private final HelperRequestService helperRequestService;
+
+    @Autowired
+    public HelpRequestController(AuthenticationFacade authenticationFacade, UserService userService, HelperRequestService helperRequestService)
+    {
+        super(userService, authenticationFacade);
+        this.helperRequestService = helperRequestService;
+    }
+
+    @PostMapping(value = "/helper-request",consumes = MediaType.APPLICATION_JSON_VALUE)
+    @ResponseBody
+    public Result<Tbl_helper_request> temp(@RequestBody Tbl_helper_request tbl_helper_request){
+        String userId = getUsername();
+        User user = userService.getUserById(userId);
+        tbl_helper_request.setPk(new TimeKey(userId, LocalDate.now(), LocalTime.now()));
+        logger.info("id:{}, type:{}, date:{}, time:{}", user.getUserId(), user.getType(), tbl_helper_request.getPk().getDate(), tbl_helper_request.getPk().getTime());
+        Result.Code code;
+        Tbl_helper_request saved = null;
+        try{
+            saved = helperRequestService.upsertHelperRequest(tbl_helper_request);
+            code = Result.Code.OK;
+        }catch (Exception exception){
+            logger.error(exception.getLocalizedMessage(), exception);
+            code = Result.Code.ERROR_DATABASE;
+        }
+        return Result.<Tbl_helper_request>builder()
+                .code(code)
+                .data(saved)
+                .build();
+    }
+}
---
+++ src/main/java/withus/dto/wwithus/HeaderInfoDTO.java
@@ -0,0 +1,48 @@
+package withus.dto.wwithus;
+
+import lombok.Builder;
+import lombok.Getter;
+import java.time.LocalDate;
+import lombok.Setter;
+
+
+public class HeaderInfoDTO {
+    private String userId;
+
+    private LocalDate birthdate;
+
+    private Integer goal;
+
+    private String name;
+
+    private Integer level;
+
+    public Integer getLevel() {
+        return level;
+    }
+
+    public String getUserId() {
+        return userId;
+    }
+
+    public LocalDate getBirthdate() {
+        return birthdate;
+    }
+
+    public Integer getGoal() {
+        return goal;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+
+    public HeaderInfoDTO(String name, String userId, LocalDate birthdate, Integer goal, Integer level) {
+        this.name = name;
+        this.userId = userId;
+        this.birthdate = birthdate;
+        this.goal = goal;
+        this.level = level;
+    }
+}
