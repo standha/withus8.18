143d4b699b8d8d0f21c258d20fb98ce744f09644
k1mjunyoung <vovrwm@gmail.com>
2023-07-26 11:58:15 +0900

    식단: 컨트롤러 넘버링으로 수정

--- src/main/java/withus/controller/DietController.java
+++ src/main/java/withus/controller/DietController.java
@@ -7,6 +7,33 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
 @RequestMapping("/diet")
 public class DietController {
+    //    soup1: 북엇국
+    //    soup2: 들깨버섯찌개
+    //    soup3: 낙지감잣국
+    //    soup4: 소고기뭇국
+    //    soup5: 단호박배추된장국
+    //    soup6: 바지락맑은국
+    //    soup7: 버섯순두부찌개
+    //    soup8: 닭곰탕
+    //
+    //    side1: 감자카레볶음
+    //    side2: 뱅어포볶음
+    //    side3: 다시다두부말이
+    //    side4: 황태조림
+    //    side5: 브로콜리견과류볶음
+    //    side6: 느타리버섯볶음
+    //    side7: 소고기채소불고기
+    //    side8: 시래기닭조림
+    //
+    //    kimchi1: 상추겉절이
+    //    kimchi2: 오이생채
+    //    kimchi3: 참외겉절이
+    //    kimchi4: 채소피클
+    //    kimchi5: 고추김치
+    //    kimchi6: 콜라비깍두기
+    //    kimchi7: 청경채김치
+    //    kimchi8: 저염겉절이
+
     // 리스트
     @GetMapping("/soup")
     public String soup() {
@@ -24,125 +51,125 @@ public class DietController {
     }
 
     // 국 상세
-    @GetMapping("/soup/북엇국")
+    @GetMapping("/soup/1")
     public String soup1() {
-        return "moistureNatrium/diet/soup/북엇국";
+        return "moistureNatrium/diet/soup/1";
     }
 
-    @GetMapping("/soup/들깨버섯찌개")
+    @GetMapping("/soup/2")
     public String soup2() {
-        return "moistureNatrium/diet/soup/들깨버섯찌개";
+        return "moistureNatrium/diet/soup/2";
     }
 
-    @GetMapping("/soup/낙지감잣국")
+    @GetMapping("/soup/3")
     public String soup3() {
-        return "moistureNatrium/diet/soup/낙지감잣국";
+        return "moistureNatrium/diet/soup/3";
     }
 
-    @GetMapping("/soup/소고기뭇국")
+    @GetMapping("/soup/4")
     public String soup4() {
-        return "moistureNatrium/diet/soup/소고기뭇국";
+        return "moistureNatrium/diet/soup/4";
     }
 
-    @GetMapping("/soup/단호박배추된장국")
+    @GetMapping("/soup/5")
     public String soup5() {
-        return "moistureNatrium/diet/soup/단호박배추된장국";
+        return "moistureNatrium/diet/soup/6";
     }
 
-    @GetMapping("/soup/바지락맑은국")
+    @GetMapping("/soup/6")
     public String soup6() {
-        return "moistureNatrium/diet/soup/바지락맑은국";
+        return "moistureNatrium/diet/soup/6";
     }
 
-    @GetMapping("/soup/버섯순두부찌개")
+    @GetMapping("/soup/7")
     public String soup7() {
-        return "moistureNatrium/diet/soup/버섯순두부찌개";
+        return "moistureNatrium/diet/soup/7";
     }
 
-    @GetMapping("/soup/닭곰탕")
+    @GetMapping("/soup/8")
     public String soup8() {
-        return "moistureNatrium/diet/soup/닭곰탕";
+        return "moistureNatrium/diet/soup/8";
     }
 
     // 반찬 상세
-    @GetMapping("/side/감자카레볶음")
+    @GetMapping("/side/1")
     public String side1() {
-        return "moistureNatrium/diet/side/감자카레볶음";
+        return "moistureNatrium/diet/side/1";
     }
 
-    @GetMapping("/side/뱅어포볶음")
+    @GetMapping("/side/2")
     public String side2() {
-        return "moistureNatrium/diet/side/뱅어포볶음";
+        return "moistureNatrium/diet/side/2";
     }
 
-    @GetMapping("/side/다시다두부말이")
+    @GetMapping("/side/3")
     public String side3() {
-        return "moistureNatrium/diet/side/다시다두부말이";
+        return "moistureNatrium/diet/side/3";
     }
 
-    @GetMapping("/side/황태조림")
+    @GetMapping("/side/4")
     public String side4() {
-        return "moistureNatrium/diet/side/황태조림";
+        return "moistureNatrium/diet/side/4";
     }
 
-    @GetMapping("/side/브로콜리견과류볶음")
+    @GetMapping("/side/5")
     public String side5() {
-        return "moistureNatrium/diet/side/브로콜리견과류볶음";
+        return "moistureNatrium/diet/side/5";
     }
 
-    @GetMapping("/side/느타리버섯볶음")
+    @GetMapping("/side/6")
     public String side6() {
-        return "moistureNatrium/diet/side/느타리버섯볶음";
+        return "moistureNatrium/diet/side/6";
     }
 
-    @GetMapping("/side/소고기채소불고기")
+    @GetMapping("/side/7")
     public String side7() {
-        return "moistureNatrium/diet/side/소고기채소불고기";
+        return "moistureNatrium/diet/side/7";
     }
 
-    @GetMapping("/side/시래기닭조림")
+    @GetMapping("/side/8")
     public String side8() {
-        return "moistureNatrium/diet/side/시래기닭조림";
+        return "moistureNatrium/diet/side/8";
     }
 
     // 김치 상세
-    @GetMapping("/kimchi/상추겉절이")
+    @GetMapping("/kimchi/1이")
     public String kimchi1() {
-        return "moistureNatrium/diet/side/상추겉절이";
+        return "moistureNatrium/diet/side/1";
     }
 
-    @GetMapping("/kimchi/오이생채")
+    @GetMapping("/kimchi/2")
     public String kimchi2() {
-        return "moistureNatrium/diet/side/오이생채";
+        return "moistureNatrium/diet/side/2";
     }
 
-    @GetMapping("/kimchi/참외겉절이")
+    @GetMapping("/kimchi/3")
     public String kimchi3() {
-        return "moistureNatrium/diet/side/참외겉절이";
+        return "moistureNatrium/diet/side/3";
     }
 
-    @GetMapping("/kimchi/채소피클")
+    @GetMapping("/kimchi/4")
     public String kimchi4() {
-        return "moistureNatrium/diet/side/채소피클";
+        return "moistureNatrium/diet/side/4";
     }
 
-    @GetMapping("/kimchi/고추김치")
+    @GetMapping("/kimchi/5")
     public String kimchi5() {
-        return "moistureNatrium/diet/side/고추김치";
+        return "moistureNatrium/diet/side/5";
     }
 
-    @GetMapping("/kimchi/콜라비깍두기")
+    @GetMapping("/kimchi/6")
     public String kimchi6() {
-        return "moistureNatrium/diet/side/콜라비깍두기";
+        return "moistureNatrium/diet/side/6";
     }
 
-    @GetMapping("/kimchi/청경채김치")
+    @GetMapping("/kimchi/7")
     public String kimchi7() {
-        return "moistureNatrium/diet/side/청경채김치";
+        return "moistureNatrium/diet/side/7";
     }
 
-    @GetMapping("/kimchi/저염겉절이")
+    @GetMapping("/kimchi/8")
     public String kimchi8() {
-        return "moistureNatrium/diet/side/저염겉절이";
+        return "moistureNatrium/diet/side/8";
     }
 }
