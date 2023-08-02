7826fc5f60b8e8af4a4d87b437b5be9aa9ac98a5
k1mjunyoung <vovrwm@gmail.com>
2023-08-02 09:41:10 +0900

    수분: 물병 추가

--- src/main/resources/static/js/moistureNatrium/moisture.js
+++ src/main/resources/static/js/moistureNatrium/moisture.js
@@ -1,3 +1,4 @@
+// 기존 코드
 function setMinus(testNum) {
 	testNum = parseInt(testNum);
 	if(testNum <= 0){
@@ -38,30 +39,28 @@ function setMinus(testNum) {
 	return false;
 }
 
-function popupClose(){
-	$("#popup").hide();
-	$("#dim").hide();
-}
-
-function popupOpen(){
-	$("#popup").show();
-	$("#dim").show();
-}
-
 function setAdd(testNum) {
 	testNum = parseInt(testNum);
 	console.log(testNum);
-	if(testNum >= 10){
+	if(testNum >= 15){
+		popupOpen();
+	}
+	else if(testNum >= 10) {
+		testNum = testNum + 1;
+		// alert("입력이 완료되었습니다.");
+		setTimeout(function (){
+			window.location.reload();
+		}, 100);
 		popupOpen();
 	}
 	else{
 		testNum = testNum + 1;
-		alert("입력이 완료되었습니다.");
+		// alert("입력이 완료되었습니다.");
 		setTimeout(function (){
 			window.location.reload();
 		}, 100);
-
 	}
+
 	const body = {
 		intake: testNum
 	};
@@ -88,16 +87,74 @@ function setAdd(testNum) {
 			}
 		});
 
-/*
-	fetch(url, options)
-		.then(response => response.json())
-		.then(data =>
-			console.log(data);
-	if
-	);*/
+	/*
+        fetch(url, options)
+            .then(response => response.json())
+            .then(data =>
+                console.log(data);
+        if
+        );*/
 
 	return false;
 }
+
+function popupOpen(){
+	$("#popup").show();
+	$("#dim").show();
+}
+
+function popupClose(){
+	$("#popup").hide();
+	$("#dim").hide();
+}
+// 기존코드
+
+// 추가코드
+function pushClick() {
+	$("#layerSelectType").show();
+	$("#dim").show();
+	$("#buttonCaregiverType").show();
+	$("#buttonRecordType").hide();
+	Item = "<span> 보호자에게 수분섭취량을 알릴까요?</span>";
+	$('#popUp2').append(Item);
+
+	$("#buttonOk").click(function () {
+		$("#layerSelectType").hide();
+		$("#dim").hide();
+		$("#buttonCaregiverType").hide();
+
+		window.location.reload();
+		return false;
+	});
+
+	$("#buttonNo").click(function () {
+		$("#layerSelectType").hide();
+		$("#dim").hide();
+		$("#buttonCaregiverType").hide();
+
+		window.location.reload();
+		return false;
+	});
+}
+
+function submit_Click() {
+
+	$("#layerSelectType").show();
+
+	$("#dim").show();
+	$("#buttonCaregiverType").hide();
+	$("#buttonRecordType").show();
+	Item2 = "<span>기록이 완료되었습니다.</span>";
+	$('#popUp2').append(Item2);
+	$("#buttonCheck").click(function () {
+		$("#layerSelectType").hide();
+		$("#dim").hide();
+		$("#buttonRecordType").hide();
+		window.location.reload();
+		return false;
+	});
+}
+// 추가코드
 
 function showHistory() {
 	const element = document.querySelector("#moisture-history-get-action");
