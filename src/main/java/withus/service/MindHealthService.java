500949b9b2fa45a22dcf16bb4b1eef76cc5d167f
nayoung03 <naje311@naver.com>
2023-08-17 18:16:42 +0900

    마음건강> 하위 기능2차 , 복용약알약오류해결

--- src/main/java/withus/service/MindHealthService.java
+++ src/main/java/withus/service/MindHealthService.java
@@ -3,6 +3,7 @@ package withus.service;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import withus.entity.RecordKey;
+import withus.entity.Tbl_Exercise_record;
 import withus.entity.Tbl_medication_alarm;
 import withus.entity.Tbl_mindHealth_record;
 import withus.repository.MindHealthRepository;
@@ -19,6 +20,24 @@ public class MindHealthService {
     public MindHealthService(MindHealthRepository mindHealthRepository){
     this.mindHealthRepository = mindHealthRepository;
     }
+    @Nullable
+    public Tbl_mindHealth_record upsertMindhealth(Tbl_mindHealth_record tbl_mindHealth_record) {
+        Tbl_mindHealth_record saved = mindHealthRepository.save(tbl_mindHealth_record);
+        return saved;
+    }
+@Nullable
+    public Tbl_mindHealth_record upsertmindText(Tbl_mindHealth_record tbl_mindHealth_record) {
+    RecordKey pk = tbl_mindHealth_record.getPk();
+    Tbl_mindHealth_record existingRecord = mindHealthRepository.findByPk(pk).orElse(null);
+        if (existingRecord != null) {
+            existingRecord.setText(tbl_mindHealth_record.getText()); // 기존 기록이 있는 경우 텍스트 업데이트
+            return mindHealthRepository.save(existingRecord);
+        } else {
+            return mindHealthRepository.save(tbl_mindHealth_record); // 기존 기록이 없는 경우 새로운 엔티티 저장
+        }
+    }
+
+
 
      @Nullable
     public Tbl_mindHealth_record getmindHealth(RecordKey pk) {
